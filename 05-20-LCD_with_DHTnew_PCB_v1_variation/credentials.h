3a3482d0ba0b656f140e6c30abe2119fd88259f3
Peter Dalmaris <peter@futureshock.com.au>
2023-03-23 12:53:42 +1100

    Add files via upload

---
+++ 05-20-LCD_with_DHTnew_PCB_v1_variation/credentials.h
@@ -0,0 +1,12 @@
+
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
+#define BLYNK_TEMPLATE_ID "---YOUR BLYNK TEMPLATE ID---"
+#define BLYNK_DEVICE_NAME "ESP01S Arduino Uno"
+#define BLYNK_AUTH_TOKEN "---YOUR BLYNK AUTH TOKEN---"
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+
+char ssid[] = "---YOUR WIFI ID---";
+char pass[] = "---YOUR WIFI PASSWORD---";
