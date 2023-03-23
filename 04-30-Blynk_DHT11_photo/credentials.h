865388056f7d80dd2b08fd237aa9284f33f515f8
Peter Dalmaris <peter@futureshock.com.au>
2023-03-23 12:16:28 +1100

    Update credentials.h

--- 04-30-Blynk_DHT11_photo/credentials.h
+++ 04-30-Blynk_DHT11_photo/credentials.h
@@ -1,7 +1,7 @@
 
 // Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
 // See the Device Info tab, or Template settings
-#define BLYNK_TEMPLATE_ID "TMPLLASN5HsJ"
+#define BLYNK_TEMPLATE_ID "--YOUR BLYNK TEMPLATE ID--"
 #define BLYNK_DEVICE_NAME "ESP01S Arduino Uno"
 #define BLYNK_AUTH_TOKEN "--YOUR BLYNK AUTH TOKEN--"
 
