f07b4eab819ad3b9669d35a5eef7714992eaeeaa
Peter Dalmaris <peter@futureshock.com.au>
2023-03-27 10:13:07 +1100

    Update credentials.h

--- 03-30-Blynk_LED_pot/credentials.h
+++ 03-30-Blynk_LED_pot/credentials.h
@@ -1,12 +1,11 @@
-
 // Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
 // See the Device Info tab, or Template settings
-#define BLYNK_TEMPLATE_ID "TMPLLCKxXYKg"
-#define BLYNK_DEVICE_NAME "Environment monitor template"
-#define BLYNK_AUTH_TOKEN "ycMW6xi6JG6yEScBk5K8KLIfMOqUx-VH"
+#define BLYNK_TEMPLATE_ID "TMPLLASN5HsJ"
+#define BLYNK_DEVICE_NAME "ESP01S Arduino Uno"
+#define BLYNK_AUTH_TOKEN "oWN3RQdMF0xDtjaO1MTds14KE8kiJeMC"
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
 
-char ssid[] = "ardwifi";
-char pass[] = "ardwifi987";
+char ssid[] = "---YOUR-WIFI-SSID---";
+char pass[] = "---YOUR-WIFI-PASSWRD---";
